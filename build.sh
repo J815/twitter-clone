0ca1f0235742563ee8e93f272b70494e506505f2
J815 <58225317+J815@users.noreply.github.com>
2024-02-03 17:29:19 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 set -o errexit
 # pip install -r requirements.txt
-
+echo "change directory"
 cd twitting
-
+echo "migrate "
 python manage.py migrate 
 
 # if [[ $CREATE_SUPERUSER ]];
 # then
+echo "create superuser "
 python manage.py createsuperuser --no-input
 # fi
